27098e3cea60dac0d013d0158d90c8f6eaf607bc
Andrew Okesokun_Dev <aokesokun_Dev@polarisbanklimited.com>
2022-02-13 21:00:18 +0100

    fix 6# Created routes and navigation handlers

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -3,27 +3,57 @@ import './App.css';
 import ProfileListComponent from '../../components/ProfileList';
 import HomeView from '../HomeView'
 import { Provider } from 'react-redux';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import store from '../../store'
+import routes from '../../routes'
+import { Nav } from 'react-bootstrap'
+
+const App: React.FC = (props) => {
+
+  const location = useLocation()
 
 
-function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-           <div className='d-flex flex-column'>
+      <Provider store={store}>
+
+        <div className="App">
+          <header className="App-header">
+            <div className='d-flex flex-column'>
               <p className="display-6">Talent Hub</p>
               <p className="lead">We have the right talent just for you</p>
-           </div>
-        </header>
-        
-        <section className='container w-80 m-auto'>
-           <HomeView />
-        </section>
-      
+            </div>
+          </header>
 
-      </div>
-    </Provider>
+          <section className='container w-80 m-auto'>
+            <Nav
+              activeKey="/home"
+            >
+              {routes.map(({ path }, key) => (
+
+                <Nav.Item key={key} className={location.pathname == path ? 'activeNav' : '' } >
+                  <Nav.Link href={path}>Home</Nav.Link>
+                </Nav.Item>
+
+              )
+              )}
+
+            </Nav>
+          </section >
+
+          <section className='container w-80 m-auto'>
+
+            <Routes>
+              {routes.map(({ Component, path, ...others }, key) =>
+                <Route key={key} path={path} element={<Component {...others} />} />)}
+
+            </Routes>
+
+
+          </section>
+        </div>
+
+      </Provider>
+
   );
 }
 
